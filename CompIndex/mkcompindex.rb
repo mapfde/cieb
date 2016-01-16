34fba86e3e7a851e44684a12256bb27a82128fb5
Tom Baker <tom@tombaker.org>
2016-01-16 13:56:53 +0100

    mkcompindex.rb  - almost there but for \n in Legend

--- CompIndex/mkcompindex.rb
+++ CompIndex/mkcompindex.rb
@@ -1,19 +1,22 @@
 #!/usr/bin/env ruby
 
 now = Time.now.strftime("%Y-%m-%d")
+header = <<-EOS
+# LD4PE Competency Index
+
+#{now}: generated from [Compindex-published.md](https://github.com/ld4pe/cieb/blob/master/CompIndex/CompIndex-draft.md)
+See also [http://explore.dublincore.net/comp-index/](http://explore.dublincore.net/comp-index/)
+
+_Legend_
+### _Topic cluster_
+   * _Topic_
+      * **_Competency: a tweet-length phrase expressing a concept or skill to be learned_**
+         * _Benchmark: specific task that demonstrates competency_
+EOS
+
 ci_raw = IO.readlines('CompIndex-test.md')
 File.open('CompIndex-test2.md', 'w') do |ci_formatted|
-  header = <<EOS
-  ci_formatted.write("# LD4PE Competency Index\n")
-  ci_formatted.write("#{now}: generated from [Compindex-published.md](https://github.com/ld4pe/cieb/blob/master/CompIndex/CompIndex-draft.md)\n\n")
-  ci_formatted.write("See also [http://explore.dublincore.net/comp-index/](http://explore.dublincore.net/comp-index/)\n\n")
-  ci_formatted.write("_Legend_")
-  ci_formatted.write("### _Topic clusters_')
-  ci_formatted.write("   * _Topics_")
-  ci_formatted.write("      * __#{line[2..-1]}__")
-  ci_formatted.write("         * #{line[2..-1]}")
-  ci_formatted.write(header)
-  EOS
+ci_formatted.write(header)
   ci_raw.each do |line| 
     case line
     when /^A/
