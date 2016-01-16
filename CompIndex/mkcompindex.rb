b9340ebded37565986e43cbbccd6bb81ce514aef
Tom Baker <tom@tombaker.org>
2016-01-16 12:38:35 +0100

    mkcompindex.rb

--- CompIndex/mkcompindex.rb
+++ CompIndex/mkcompindex.rb
@@ -1,18 +1,19 @@
 #!/usr/bin/env ruby
 
-compindexraw = IO.readlines("CompIndex-test.md")
-compindex_formatted = []
-compindexraw.each do |line| 
-  case line
-  when /^A/
-    compindex_formatted << line
-  when /^B/
-    compindex_formatted << "* #{line}"
-  when /^C/
-    compindex_formatted << "  * #{line}"
-  when /^D/
-    compindex_formatted << "    * #{line}"
+compindex_raw = IO.readlines('CompIndex-test.md')
+File.open('CompIndex-test2.md', 'w') do |compindex_formatted|
+  compindex_raw.each do |line| 
+    case line
+    when /^A/
+      compindex_formatted.write("\n### #{line}\n")
+    when /^B/
+      compindex_formatted.write("   * #{line}")
+    when /^C/
+      compindex_formatted.write("      * #{line}")
+    when /^D/
+      compindex_formatted.write("         * #{line}")
+    end
   end
 end
 
-compindex_formatted.each { |line| puts line }
+# pandoc CompIndex-test2.md -o CompIndex-test2.docx
