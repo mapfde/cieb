4ff01bb57e10cdc9bd44542c5cbb1047e4b1d3cc
Tom Baker <tom@tombaker.org>
2016-01-16 13:10:18 +0100

    mkcompindex.rb

--- CompIndex/mkcompindex.rb
+++ CompIndex/mkcompindex.rb
@@ -1,19 +1,21 @@
 #!/usr/bin/env ruby
 
-compindex_raw = IO.readlines('CompIndex-test.md')
-File.open('CompIndex-test2.md', 'w') do |compindex_formatted|
-  compindex_raw.each do |line| 
+now = Time.now.strftime("%Y-%m-%d")
+ci_raw = IO.readlines('CompIndex-test.md')
+File.open('CompIndex-test2.md', 'w') do |ci_formatted|
+  ci_formatted.write("## LD4PE Competency Index\n")
+  ci_formatted.write("#{now}: Generated from [Compindex-published.md](https://github.com/ld4pe/cieb/blob/master/CompIndex/CompIndex-draft.md)\n\n")
+  ci_raw.each do |line| 
     case line
     when /^A/
-      compindex_formatted.write("\n### #{line}\n")
+      ci_formatted.write("\n### #{line}\n")
     when /^B/
-      compindex_formatted.write("   * #{line}")
+      ci_formatted.write("   * #{line}")
     when /^C/
-      compindex_formatted.write("      * #{line}")
+      ci_formatted.write("      * #{line}")
     when /^D/
-      compindex_formatted.write("         * #{line}")
+      ci_formatted.write("         * #{line}")
     end
   end
 end
 
-# pandoc CompIndex-test2.md -o CompIndex-test2.docx
