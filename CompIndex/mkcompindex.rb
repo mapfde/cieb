87e0e32e568735445d2590e106fa690c0472ac49
Tom Baker <tom@tombaker.org>
2016-01-16 14:59:31 +0100

    This works

--- CompIndex/mkcompindex.rb
+++ CompIndex/mkcompindex.rb
@@ -1,32 +1,33 @@
 #!/usr/bin/env ruby
 
 now = Time.now.strftime("%Y-%m-%d")
-header = <<-EOS
+header = %(
 # LD4PE Competency Index
 
-#{now}: generated from [Compindex-published.md](https://github.com/ld4pe/cieb/blob/master/CompIndex/CompIndex-draft.md)
-See also [http://explore.dublincore.net/comp-index/](http://explore.dublincore.net/comp-index/)
+#{now}: generated from [Compindex-published.md](https://github.com/ld4pe/cieb/blob/master/CompIndex/CompIndex-draft.md)\n
+See also [http://explore.dublincore.net/comp-index/](http://explore.dublincore.net/comp-index/)\n\n
 
-_Legend_
-### _Topic cluster_
-   * _Topic_
-      * **_Competency: a tweet-length phrase expressing a concept or skill to be learned_**
-         * _Benchmark: specific task that demonstrates competency_
-EOS
+-----------------------------------\n
+### Topic cluster
+   * Topic
+      * Competency: a tweet-length phrase expressing a concept or skill to be learned
+         * Benchmark: specific task that demonstrates competency\n
+-----------------------------------\n
+)
 
-ci_raw = IO.readlines('CompIndex-test.md')
-File.open('CompIndex-test2.md', 'w') do |ci_formatted|
-ci_formatted.write(header)
+ci_raw = IO.readlines('CompIndex-test.txt')
+File.open('CompIndex-test2.md', 'w') do |file|
+  file.write(header)
   ci_raw.each do |line| 
     case line
     when /^A/
-      ci_formatted.write("\n### #{line[2..-1]}\n")
+      file.write("\n### #{line[2..-1]}\n")
     when /^B/
-      ci_formatted.write("   * #{line[2..-1]}")
+      file.write("   * #{line[2..-1]}")
     when /^C/
-      ci_formatted.write("      * __#{line[2..-1]}__")
+      file.write("      * #{line[2..-1]}")
     when /^D/
-      ci_formatted.write("         * #{line[2..-1]}")
+      file.write("         * #{line[2..-1]}")
     end
   end
 end
